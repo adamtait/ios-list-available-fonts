aa0e80dd534aa2a7c9316c7292a259272e66de91
Adam Tait <bin@adamta.it>
2019-01-10 23:16:57 -0800

    add first working version of list fonts tool for iOS

---
+++ ListAvailableFonts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ListAvailableFonts
+//
+//  Created by Adam Tait on 8/20/18.
+//  Copyright © 2018 Sisterical Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ListAvailableFonts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ListAvailableFonts
+//
+//  Created by Adam Tait on 8/20/18.
+//  Copyright © 2018 Sisterical Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
